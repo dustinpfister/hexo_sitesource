bc948420134097815104077ea3484a3b4dedf58f
dustinpfister <dustin.pfister@gmail.com>
2017-10-22 14:03:49 -0400

    phaser tags

---
+++ scripts/phaser-tags.js
@@ -0,0 +1,25 @@
+/*
+
+These are some tags that just apply to my phaser posts
+
+ */
+
+// standard if new to phaser message
+hexo.extend.tag.register('phaser_if_new_mess', function(){
+
+    return '<h2 id=\"If new to Phaser\"><a href=\"#If-new-to-Phaser\" class=\"headerlink\" title=\"If new to Phaser\"></a>If new to Phaser<\/h2><p>If you are new to phaser you might want to check out my post on <a href=\"/2017/10/04/phaser-getting-started/\">getting started with phaser<\/a>, I also assume that you have at least some knowlage of html, css, and javaScript as I do not have any posts on how to get up to speed with that at this time. This is my standard <a href=\"/2017/02/04/hexo-tags/\">hexo tag<\/a> for all <a href=\"/categories/phaser/\">phaser posts<\/a> where I want to inform readers that this is not a post for beginers.<\/p>';
+
+});
+ 
+hexo.extend.tag.register('phaser_top', function () {
+
+    return '';
+
+});
+
+// standard bottom text
+hexo.extend.tag.register('phaser_bottom', function () {
+
+    return '';
+
+});
